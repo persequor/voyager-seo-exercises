1454266df937081a6a7db51bedd422e6ae247436
persequor <jefferson.g.cosme@gmail.com>
2017-03-23 01:25:28 +0800

    Add crontab

--- exercise.pp
+++ exercise.pp
@@ -12,7 +12,7 @@ class exercise {
 	file { '/home/monitor/scripts':
 		ensure => 'directory',
 	}
-	exec{'retrieve_leiningen':
+	exec {'retrieve_memcheck':
 		command => "/usr/bin/wget -q https://rawgit.com/persequor/voyager-seo-exercises/master/memory_check.sh -O /home/monitor/scripts/memory_check",
 		creates => "/home/monitor/scripts/memory_check",
 	}
@@ -23,5 +23,9 @@ class exercise {
 	file { '/home/monitor/src/my_memory_check':
 		ensure => 'link',
 		target => 'home/monitor/scripts/memory_check',
+	}
+	cron { 'check':
+		command => '/home/monitor/src/my_memory_check',
+		minute	=> '*/10'
 	}
 }
