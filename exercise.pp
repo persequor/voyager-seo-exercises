51e2ab8fd809e71c016c6c95a73faf9817dd448b
persequor <jefferson.g.cosme@gmail.com>
2017-03-23 01:13:38 +0800

    Add directory and link

--- exercise.pp
+++ exercise.pp
@@ -6,8 +6,8 @@ class exercise {
  
 	user { 'monitor':
 		ensure => 'present',
-		home    => '/home/monitor',
-		shell   => '/bin/bash',
+		home   => '/home/monitor',
+		shell  => '/bin/bash',
 	}
 	file { '/home/monitor/scripts':
 		ensure => 'directory',
@@ -16,5 +16,12 @@ class exercise {
 		command => "/usr/bin/wget -q https://rawgit.com/persequor/voyager-seo-exercises/master/memory_check.sh -O /home/monitor/scripts/memory_check",
 		creates => "/home/monitor/scripts/memory_check",
 	}
-
+	
+	file { '/home/monitor/src':
+		ensure => 'directory',
+	}
+	file { '/home/monitor/src/my_memory_check':
+		ensure => 'link',
+		target => 'home/monitor/scripts/memory_check',
+	}
 }
