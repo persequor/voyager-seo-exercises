71c806c193dacaa8ef5cd16dd38e34be60271b9c
persequor <jefferson.g.cosme@gmail.com>
2017-03-23 00:57:33 +0800

    Add user monitor

--- exercise.pp
+++ exercise.pp
@@ -1,8 +1,13 @@
-class packages {
-  Package { ensure => 'installed' }
-
-  package { 'vim': }
-  package { 'curl': }
-  package { 'git':   }
+class exercise {
+	Package { ensure => 'installed' }
+	package { 'vim': }
+	package { 'curl': }
+	package { 'git':   }
+ 
+	user { 'monitor':
+		ensure => 'present',
+		home    => '/home/monitor',
+		shell   => '/bin/bash',
+	}
 
 }
